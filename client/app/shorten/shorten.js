8d5b96e7a351cd937e453c6ea950af819cf931ea
Jisoo Yoon <jyoon006@gmail.com>
2016-02-12 18:40:55 -0500

    all test passed for shorten

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,4 +2,13 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
+  // angular.extend($scope, $location, Links);
+  $scope.link = {};
+
+  $scope.addLink = function() {
+    Links.addLink(function(data) {
+      console.log('addlink data', data);
+    });
+  };
+  
 });
