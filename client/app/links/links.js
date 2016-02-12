944f125513989eff1a706bcc109e73f37f114825
Jisoo Yoon <jyoon006@gmail.com>
2016-02-12 15:35:32 -0500

    added factory to return data object and getLinks function - test passed

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,17 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  angular.extend($scope, Links);
+})
+.factory('Links', function() {
+  var data = [];
+
+  return {
+    data: data,
+    getLinks: function() {
+      
+    }
+  }
 });
+
+
