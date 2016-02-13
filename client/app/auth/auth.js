40413e5bd8ad55ddc9f78ae14210b95cfbf32dd8
Jisoo Yoon <jyoon006@gmail.com>
2016-02-13 15:20:30 -0500

    able to show error when submitting invalid username or pw

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -14,6 +14,7 @@ angular.module('shortly.auth', [])
         $location.path('/links');
       })
       .catch(function (error) {
+        $scope.errorLogin = true;
         console.error(error);
       });
     Auth.isAuth();
